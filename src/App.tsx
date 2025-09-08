1b9ab4b3b28c91288d6abe43138b37f4fbe01c1a
Roman2508 <2508romaa@gmail.com>
2025-09-08 23:40:57 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react";
-import { Repeat as RepeatIcon } from "lucide-react";
+import {
+  Check as CheckIcon,
+  Languages,
+  Repeat as RepeatIcon,
+} from "lucide-react";
 import { useDebounceValue } from "@siberiacancode/reactuse";
 
 import { cn } from "./lib/utils";
@@ -21,18 +25,16 @@ type DictionaryType = {
   createdAt: string;
 };
 
-// const onCreateNewDictItem = () => {
-//   axios.post(`${import.meta.env.VITE_BASE_URL}/dictionary`, {
-//     latin: 'test_latin',
-//     ukrainian: 'test_ukrainian',
-//   })
-// }
-// const onUpdateDictItem = () => {
-//   axios.patch(`${import.meta.env.VITE_BASE_URL}/dictionary/4`, {
-//     latin: '_latin_',
-//     ukrainian: 'test_ukrainian',
-//   })
-// }
+const categories = [
+  "Назви рослин",
+  "Алфавітний покажчик ботанічних назв",
+  "Назви лікарських форм",
+  "Рецептурні вирази, рецептурні скорочення",
+  "Частотні відрізки",
+  "Клінічна термінологія",
+  "Хімічна номенклатура",
+  "Крилаті фрази",
+];
 
 function App() {
   const [translationType, setTranslationType] = useState<"uk_la" | "la_uk">(
@@ -40,7 +42,7 @@ function App() {
   );
 
   const [search, setSearch] = useState("");
-  // const [currentPage, setCurrentPage] = useState(1)
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [selectedLetter, setSelectedLetter] = useState<string | null>(null);
 
   const debouncedSearch = useDebounceValue(search, 500);
@@ -64,177 +66,160 @@ function App() {
   }, [debouncedSearch]);
 
   return (
-    <div
-      className="min-h-screen bg-[radial-gradient(circle,_rgba(0,0,0,0.15)_1px,_transparent_1px)] [background-size:16px_16px] flex justify-center flex-col px-4 overflow-hidden"
-      // style={{
-      //   background: "linear-gradient(111.34deg, #a0bfe4 0, #07f 100%)",
-      // }}
-    >
-      {/* style={{ color: '#1d5d9b' }} */}
+    <div className="min-h-screen">
+      <div className="border-b flex justify-center p-2 text-lg">
+        Латинсько-український словник фармацевтично-медичних термінів
+      </div>
 
-      {/* <div className="absolute top-1 left-4 flex items-center gap-2">
-        <Logo variant="color" />
-        <div>
-          <h1 className="font-extrabold text-lg leading-none">ЖБФФК</h1>
-          <p className="text-sm opacity-[0.7]">Українсько-Латинський словник</p>
-        </div>
-      </div> */}
-
-      {/* <svg
-        width="100%"
-        height="100%"
-        viewBox="0 0 1266 900"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        preserveAspectRatio="xMidYMax slice"
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          maxHeight: "100vh",
-          overflow: "hidden",
-          color: "#fff",
-          transform: "rotate(180deg)",
-          zIndex: 0,
-        }}
-      >
-        <path
-          d="M626.614 0V484.725M626.614 484.725V900C138.607 900 1 153.071 1 0M626.614 484.725C1063.56 484.725 1159.45 175.749 1202.58 0M626.614 484.725C1011.06 484.725 1216.2 785.481 1265 900"
-          stroke="currentColor"
-          stroke-width="1.60111"
-        ></path>
-      </svg> */}
-
-      <header
-        // style={{ backgroundColor: "#fff" }}
-        style={{ backgroundColor: "#1d5d9b" }}
-        className="mx-auto w-full max-w-3xl mb-4 px-4 flex items-center gap-2 p-2 rounded-b-sm text-white" // text-white
-      >
-        <Logo variant="color" />
-        <div>
-          <h1 className="font-extrabold text-lg leading-none">ЖБФФК</h1>
-          <p className="text-sm opacity-[0.7]">Українсько-Латинський словник</p>
-        </div>
-      </header>
-
-      <Card>
-        <div className="flex justify-between items-center gap-8 w-full flex-col-reverse md:flex-row">
-          <Input
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            className="w-60"
-            placeholder="Знайти..."
-          />
+      <div className="sticky top-0 z-10">
+        <header className="flex justify-between h-[70px] w-full max-w-5xl rounded-full bg-[hsla(0,0%,93%,0.72)] backdrop-blur-xl absolute left-1/2 top-4 z-10 -translate-x-1/2 items-center gap-x-24 pl-6 pr-3 py-2">
+          <div className="flex items-center gap-2">
+            <Logo variant="color" />
+            <h1 className="font-bold text-lg leading-none text-primary">
+              ЖБФФК
+            </h1>
+          </div>
 
           <div className="flex gap-2">
-            <Button
-              variant="outline"
-              className="w-28 cursor-default hover:bg-background"
-            >
-              {translationType === "la_uk" ? "Латинська" : "Український"}
-            </Button>
+            <button className="rounded-full h-11 px-4 bg-primary text-white font-semibold cursor-pointer">
+              Словник
+            </button>
 
-            <Button variant="secondary" onClick={onChangeTranslationType}>
-              <RepeatIcon />
-            </Button>
-
-            <Button
-              variant="outline"
-              className="w-28 cursor-default hover:bg-background"
-            >
-              {translationType === "la_uk" ? "Українська" : "Латинська"}
-            </Button>
+            <button className="rounded-full h-11 px-4 text-primary font-semibold cursor-pointer">
+              Тренажер
+            </button>
           </div>
+        </header>
+      </div>
+
+      <div className="pt-30 pb-10 max-w-5xl mx-auto flex items-baseline gap-8">
+        <div className="w-80 sticky top-24">
+          {categories.map((el) => (
+            <div
+              onClick={() =>
+                setSelectedCategory(el === selectedCategory ? "" : el)
+              }
+              key={el}
+              className={cn(
+                "flex justify-between items-center mb-0 py-3 px-0 border-b cursor-pointer",
+                selectedCategory === el ? "font-bold text-primary" : ""
+              )}
+            >
+              <p className="text-sm">{el}</p>
+            </div>
+          ))}
         </div>
 
-        {/* <div className="flex gap-2">
-          <Button onClick={onCreateNewDictItem}>Create New Entity</Button>
-          <Button onClick={onUpdateDictItem}>Update Entity</Button>
-        </div> */}
+        <Card>
+          <div className="flex justify-between items-center gap-8 w-full flex-col-reverse md:flex-row">
+            <Input
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              className="w-60"
+              placeholder="Знайти..."
+            />
 
-        {/* <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} /> */}
+            <div className="">
+              <div className="flex gap-2">
+                <Button
+                  variant="outline"
+                  className="w-28 cursor-default hover:bg-background rounded-full"
+                >
+                  {translationType === "la_uk" ? "Латинська" : "Український"}
+                </Button>
 
-        <div className="">
-          {(translationType === "la_uk" ? LAT_ALPHABET : UKR_ALPHABET).map(
-            (letter) => (
-              <Button
-                key={letter}
-                className="w-[42px]"
-                variant={selectedLetter === letter ? "default" : "ghost"}
-                onClick={() => {
-                  if (selectedLetter === letter) {
-                    setSelectedLetter(null);
-                  } else {
-                    setSelectedLetter(letter);
-                  }
-                }}
-              >
-                {letter}
+                <Button
+                  variant="secondary"
+                  onClick={onChangeTranslationType}
+                  className="rounded-full"
+                >
+                  <RepeatIcon />
+                </Button>
+
+                <Button
+                  variant="outline"
+                  className="w-28 cursor-default hover:bg-background rounded-full"
+                >
+                  {translationType === "la_uk" ? "Українська" : "Латинська"}
+                </Button>
+              </div>
+
+              <Button className="rounded-full" variant="secondary">
+                <Languages />
               </Button>
-            )
-          )}
-        </div>
-
-        <div className="w-full">
-          <div className="grid grid-cols-2 gap-y-2 font-bold border-b">
-            <div className="flex gap-4">
-              <div className="flex-1">
-                {translationType === "la_uk" ? "Латинська" : "Українська"}
-              </div>
-              <div className="flex-1">
-                {translationType === "la_uk" ? "Українська" : "Латинська"}
-              </div>
-            </div>
-            <div className="flex gap-4 pl-4">
-              {/* <div className="flex gap-4 border-l pl-4"> */}
-              <div className="flex-1">
-                {translationType === "la_uk" ? "Латинська" : "Українська"}
-              </div>
-              <div className="flex-1">
-                {translationType === "la_uk" ? "Українська" : "Латинська"}
-              </div>
             </div>
           </div>
 
-          <div className="max-h-[calc(100vh-350px)] overflow-auto">
-            {!query.isFetching && !query.data.docs.length && (
-              <p className="py-6 text-center">Нічого не знайдено</p>
-            )}
-
-            {query.isFetching ? (
-              <p className="py-6 text-center">Завантаження...</p>
-            ) : (
-              <div className="grid grid-cols-2">
-                {query.data?.docs.map((dict: DictionaryType, index: number) => {
-                  const showStyle = Math.floor(index / 2) % 2 === 0;
-                  return (
-                    <div
-                      key={dict.id}
-                      className={cn(
-                        "flex gap-4 p-1 even:border-l even:pl-4",
-                        showStyle ? "" : "bg-gray-50"
-                      )}
-                    >
-                      <div className="flex-1">
-                        {translationType === "la_uk"
-                          ? dict.latin
-                          : dict.ukrainian}
-                      </div>
-                      <div className="flex-1 border-gray-300">
-                        {translationType === "la_uk"
-                          ? dict.ukrainian
-                          : dict.latin}
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
+          <div className="my-4">
+            {(translationType === "la_uk" ? LAT_ALPHABET : UKR_ALPHABET).map(
+              (letter) => (
+                <Button
+                  key={letter}
+                  className="w-[39px]"
+                  variant={selectedLetter === letter ? "default" : "ghost"}
+                  onClick={() => {
+                    if (selectedLetter === letter) {
+                      setSelectedLetter(null);
+                    } else {
+                      setSelectedLetter(letter);
+                    }
+                  }}
+                >
+                  {letter}
+                </Button>
+              )
             )}
           </div>
-        </div>
-      </Card>
+
+          <div>
+            <div className="gap-y-2 font-bold border-b">
+              <div className="flex gap-4">
+                <div className="flex-1">
+                  {translationType === "la_uk" ? "Латинська" : "Українська"}
+                </div>
+                <div className="flex-1">
+                  {translationType === "la_uk" ? "Українська" : "Латинська"}
+                </div>
+              </div>
+            </div>
+
+            <div className="">
+              {!query.isFetching && !query.data.docs.length && (
+                <p className="py-6 text-center">Нічого не знайдено</p>
+              )}
+
+              {query.isFetching ? (
+                <p className="py-6 text-center">Завантаження...</p>
+              ) : (
+                <div className="grid grid-cols-1">
+                  {query.data?.docs.map((dict: DictionaryType) => {
+                    return (
+                      <div
+                        key={dict.id}
+                        className={cn(
+                          "flex gap-4 p-1 border-b"
+                          // "flex gap-4 p-1 even:bg-[hsla(0,0%,93%,0.72)]",
+                        )}
+                      >
+                        <div className="flex-1">
+                          {translationType === "la_uk"
+                            ? dict.latin
+                            : dict.ukrainian}
+                        </div>
+                        <div className="flex-1 border-gray-300">
+                          {translationType === "la_uk"
+                            ? dict.ukrainian
+                            : dict.latin}
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              )}
+            </div>
+          </div>
+        </Card>
+      </div>
     </div>
   );
 }
--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -2,7 +2,7 @@ import { type FC, type PropsWithChildren } from "react";
 
 const Card: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="flex flex-col items-center gap-8 container max-w-3xl relative mx-auto mb-8 flex-1 p-6 border border-border backdrop-blur-[4px] rounded-lg">
+    <div className="flex-1">
       {/* bg-white */}
       {children}
     </div>
